cea7a9e7c0e7d18b117d134312dae4623a147df3
Amiina-dev <88128558+Amiina-dev@users.noreply.github.com>
2022-10-18 11:56:15 +0100

    Update

--- utils.c
+++ utils.c
@@ -3,9 +3,9 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- *
  * Return: 1 if c is printable, 0 otherwise
  */
+
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
