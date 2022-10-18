0b6b56f6076b465a45f697efaa932a30e8a661c4
Amiina-dev <88128558+Amiina-dev@users.noreply.github.com>
2022-10-18 19:08:43 +0100

    Updated

--- get_flag.c
+++ get_flag.c
@@ -5,9 +5,9 @@
  * a flag modifier in the format string
  * @s: character that holds the flag specifier
  * @f: pointer to the struct flags in which we turn the flags on
- *
  * Return: 1 if a flag has been turned on, 0 otherwise
  */
+
 int get_flag(char s, flags_t *f)
 {
 	int i = 0;
